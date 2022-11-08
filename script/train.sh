16bbba4b1400cd9588c928c5ed28874771a6f610
S-Haruya <suzuki@ai.cs.ehime-u.ac.jp>
2022-11-09 00:50:50 +0900

    Update Files

--- script/train.sh
+++ script/train.sh
@@ -1,12 +1,6 @@
 # k=10 (LB:0.629)
-# python src/train.py \
-#     --model_name ku-nlp/roberta-base-japanese-char-wwm \
-#     --batch_size 16
-#     --epochs 100
-
-# k=15 (LB:0.629)
+# k=15 (LB:?)
 python src/train.py \
     --model_name ku-nlp/roberta-base-japanese-char-wwm \
-    --batch_size 32
-    --learning_rate 2e-5
+    --batch_size 16
     --epochs 100
--- src/submission.py
+++ src/submission.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 def main():
     preds = []
-    for i in range(10):
+    for i in range(15):
         preds.append(pd.read_csv(f"./data/outputs/sub_{str(i)}.csv"))
 
-    result = (preds[0]["label"] + preds[1]["label"] + preds[2]["label"] + preds[3]["label"] + preds[4]["label"] + preds[5]["label"] + preds[6]["label"] + preds[7]["label"] + preds[8]["label"] + preds[9]["label"]) / 10
+    result = (preds[0]["label"] + preds[1]["label"] + preds[2]["label"] + preds[3]["label"] + preds[4]["label"] + preds[5]["label"] + preds[6]["label"] + preds[7]["label"] + preds[8]["label"] + preds[9]["label"] + preds[10]["label"] + preds[11]["label"] + preds[12]["label"] + preds[13]["label"] + preds[14]["label"]) / 15
     preds[0]["label"] = round(result).astype(int)
 
     preds[0].to_csv("./data/outputs/sub.csv", index=None)
