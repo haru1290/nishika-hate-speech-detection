bd3312dbdaf0e81220df4693035b255cf2df62e4
S-Haruya <suzuki@ai.cs.ehime-u.ac.jp>
2022-11-08 18:54:42 +0900

    Update Files

--- script/process_all001.sh
+++ script/process_all001.sh
@@ -1,6 +1,6 @@
-# python src/train.py \
-#     --model_name ku-nlp/roberta-base-japanese-char-wwm
-
 python src/train.py \
-    --model_name izumi-lab/bert-small-japanese
+    --model_name ku-nlp/roberta-base-japanese-char-wwm
+
+# python src/train.py \
+#     --model_name izumi-lab/bert-small-japanese
     
