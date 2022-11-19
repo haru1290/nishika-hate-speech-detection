fee7b3688054b5318b37f6179efbb5f69b3ee226
S-Haruya <suzuki@ai.cs.ehime-u.ac.jp>
2022-11-20 01:49:24 +0900

    Update Files

--- script/train007.sh
+++ script/train007.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES="2,3"
 
 # luke-japanese-largeでfoldを減らして実験10fold
-# (CV: 0.?, LB: ?)
+# (CV: ?, LB: ?)
 python src/train001.py \
     --model_name studio-ousia/luke-japanese-large \
     --run_name train007 \
