90368b22d027aa101ee5d12dad11e3dae944de7c
S-Haruya <suzuki@ai.cs.ehime-u.ac.jp>
2022-11-20 15:24:03 +0900

    Update Files

--- script/train009.sh
+++ script/train009.sh
@@ -1,11 +1,11 @@
 export CUDA_VISIBLE_DEVICES="0,1,2,3"
 
-# luke-japanese-large-lite (k=10)
-# (CV: 0.754, LB: ?)
+# luke-japanese-large-lite (k=14)
+# (CV: ?, LB: ?)
 python src/train001.py \
     --model_name studio-ousia/luke-japanese-large-lite \
-    --run_name train008 \
-    --k_fold 10 \
+    --run_name train009 \
+    --k_fold 14 \
     --max_length 256 \
     --batch_size 16 \
     --epochs 100 \
