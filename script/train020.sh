0dee4be5bd7d887bf2b55df6cd5fe464de817440
S-Haruya <suzuki@ai.cs.ehime-u.ac.jp>
2022-11-24 04:17:13 +0900

    Update Files

--- script/train020.sh
+++ script/train020.sh
@@ -3,7 +3,7 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3"
 # studio-ousia/luke-japanese-large
 # ソフトラベルの追加
 # ラベルスムージング0.1
-# (CV: ?, LB: ?)
+# (CV: 0.749, LB: 0.763)
 python src/train006.py \
     --model_name studio-ousia/luke-japanese-large \
     --run_name train020 \
