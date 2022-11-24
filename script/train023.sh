cd74593bfd2fa252f501224a1506701d32f26353
S-Haruya <suzuki@ai.cs.ehime-u.ac.jp>
2022-11-25 00:11:01 +0900

    Update Files

---
+++ script/train023.sh
@@ -0,0 +1,14 @@
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
+
+# studio-ousia/luke-japanese-large
+# ソフトラベルの追加
+# ラベルスムージング0.01
+# (CV: ?, LB: ?)
+python src/train009.py \
+    --model_name studio-ousia/luke-japanese-large \
+    --run_name train023 \
+    --k_fold 10 \
+    --max_length 256 \
+    --batch_size 16 \
+    --epochs 100 \
+    
