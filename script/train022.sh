6f037971b9c438645ab1d2e543ac94154a343278
S-Haruya <suzuki@ai.cs.ehime-u.ac.jp>
2022-11-25 03:02:57 +0900

    update Files

--- script/train022.sh
+++ script/train022.sh
@@ -3,7 +3,7 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3"
 # studio-ousia/luke-japanese-large
 # ソフトラベルの追加
 # ラベルスムージング0.9
-# (CV: 0.756, LB: ?)
+# (CV: 0.756, LB: 0.702)
 python src/train008.py \
     --model_name studio-ousia/luke-japanese-large \
     --run_name train022 \
--- script/train023.sh
+++ script/train023.sh
@@ -3,7 +3,7 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3"
 # studio-ousia/luke-japanese-large
 # ソフトラベルの追加
 # ラベルスムージング0.01
-# (CV: ?, LB: ?)
+# (CV: 0.695, LB: ?)
 python src/train009.py \
     --model_name studio-ousia/luke-japanese-large \
     --run_name train023 \
