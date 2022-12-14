f7ffd7e04e642dc0f91df3b74e1b95e3335349a1
S-Haruya <suzuki@ai.cs.ehime-u.ac.jp>
2022-12-14 20:55:24 +0900

    Update src

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,24 +1,3 @@
 # データのダウンロード
 
 import pandas as pd
-from torch.utils.data import Dataset
-
-
-class HateSpeechDataset(Dataset):
-    def __init__(self, X, y=None):
-        self.X = X
-        self.y = y
-
-    def __len__(self):
-        return len(self.X)
-
-    def __getitem__(self, index):
-        input = {
-            "input_ids": self.X[index]["input_ids"],
-            "attention_mask": self.X[index]["attention_mask"],
-        }
-
-        if self.y is not None:
-            input["labels"] = self.y[index]
-
-        return input
