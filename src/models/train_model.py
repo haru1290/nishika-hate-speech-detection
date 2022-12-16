ffe4ecb49fbcb1b65ffe37953d6dc9bf91f729c0
S-Haruya <suzuki@ai.cs.ehime-u.ac.jp>
2022-12-17 05:59:42 +0900

    Update train_model.py

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -83,12 +83,10 @@ def train(train_df, cfg):
     for fold_index, (train_index, valid_index) in enumerate(skf.split(train_df["text"].values, train_df["label"].values)):
         train_data = train_df.iloc[train_index]
         valid_data = train_df.iloc[valid_index]
-
         train_dataset = Dataset.from_pandas(train_data)
         valid_dataset = Dataset.from_pandas(valid_data)
-
-        train_tokenized_dataset = train_dataset.map(tokenizer_function, batched=True)
-        valid_tokenized_dataset = valid_dataset.map(tokenizer_function, batched=True)
+        train_dataset = train_dataset.map(tokenizer_function, batched=True)
+        valid_dataset = valid_dataset.map(tokenizer_function, batched=True)
 
         model = AutoModelForSequenceClassification.from_pretrained("studio-ousia/luke-japanese-large")
 
